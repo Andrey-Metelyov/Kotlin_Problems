1c212c8c4d804293f6f605fbcb8d9942db29fdd8
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-09-05 03:09:51 +0900

    The index of the first max

--- Topics/For loop and lists/The index of the first max/src/Main.kt
+++ Topics/For loop and lists/The index of the first max/src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
     // write your code here
+    val n = readLine()!!.toInt()
+    var max = readLine()!!.toInt()
+    var index = 0
+    for (i in 1 until n) {
+        val value = readLine()!!.toInt()
+        if (value > max) {
+            max = value
+            index = i
+        }
+    }
+    println(index)
 }
