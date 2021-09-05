4503828a76ae9d46d579a956e80abe8a081a268d
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-09-05 13:16:49 +0900

    Count occurrences

---
+++ Topics/For loop and lists/Count occurrences/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    // write your code here
+    val list = List(readLine()!!.toInt()) { readLine()!!.toInt() }
+    val m = readLine()!!.toInt()
+    println(list.count { it == m } )
+}
